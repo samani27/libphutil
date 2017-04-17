c3c0717721d6f1f808176d70168abc9bffa562b5
samani <samani@mail.ugm.ac.id>
2017-04-17 10:43:09 +0700

    exception for symfony container

--- src/__phutil_library_init__.php
+++ src/__phutil_library_init__.php
@@ -10,7 +10,7 @@ require_once $root.'/moduleutils/PhutilLibraryConflictException.php';
 
 function __phutil_autoload($class_name) {
   // Occurs in PHP 5.2 with `call_user_func(array($this, 'self::f'))`.
-  if ($class_name === 'self' || $class_name === 'parent') {
+  if ($class_name === 'self' || $class_name === 'parent' || $class_name ==='Container') {
     return;
   }
 
